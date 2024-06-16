fc31f687e89b3ea3b0079d1745b85c089e98f57c
Maxim Hallasaar <endermax200113@mail.ru>
2024-06-16 08:48:08 +0300

    The package manager was switched to Yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import './assets/sass/App.sass'
 import Header from './components/header/Header'
 
-const App: React.FC = (): JSX.Element => {
+const App: React.FC = (): React.JSX.Element => {
 	return (
-		<div className="App">
-			<Header real />
+		<div className='app'>
 			<Header />
-
-			<main className="main">
-				
-			</main>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import './../../assets/sass/App.sass'
 import './Header.sass'
-import Logo from './Logo'
-import Navbar from './Navbar'
 
-interface PropsHeader {
-	real?: boolean
-}
-
-const Header: React.FC<PropsHeader> = ({real}): JSX.Element => {
-	return real 
-		? (<header className='header'>
-			<div className='container container--space-between'>
-				<Logo />
-				<Navbar />
-			</div>
-		</header>)
-		: (<div className='header-false'></div>)
+const Header: React.FC = (): React.JSX.Element => {
+	return <header className='header'></header>
 }
 
 export default Header
