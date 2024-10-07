28ca83c5278b4c5291e4f2a5a504e6c5d891e026
Max Hallasaar <endermax200113@mail.ru>
2024-10-07 07:14:06 +0300

    Added component 'Skills'

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,29 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import sass from './Skills.module.sass'
 import Headline from '@ui/text/headline/Headline'
+import Image from '@ui/image/Image'
+import Label from '@ui/text/label/Label'
+import SVGImage from 'SVGImage'
 
 interface PropsSkills {}
 
+type Skill = [url: string | SVGImage, name: string]
+type AllSkills = Skill[]
+type FourSkills = AllSkills[]
+type BlockSkills = [title: string, arrFourSkills: FourSkills]
+
 const Skills: React.FC<PropsSkills> = () => {
+	const [arrBlocksSkills, setArrBlocksSkills] = useState<BlockSkills[]>([])
+
+	useEffect(() => {
+		const arrBlocks: BlockSkills[] = []
+		let arrSkills: AllSkills = []
+		let arrFourSkills: FourSkills = []
+
+		const distributeSkills = (title: string): void => {
+			let fourSkills: AllSkills = []
+
+			arrSkills.forEach(([url, name], i, arr) => {
+				const skill: Skill = [url, name]
+
+				fourSkills.push(skill)
+
+				if (i % 4 === 3 || arr.length === i + 1) {
+					arrFourSkills.push(fourSkills)
+					fourSkills = []
+				}
+			})
+
+			arrBlocks.push([title, arrFourSkills])
+			arrSkills = []
+			arrFourSkills = []
+		}
+
+		arrSkills.push([require('@svg/skills/html.svg'), 'HTML5'])
+		arrSkills.push([require('@svg/skills/pug.svg'), 'Pug'])
+		arrSkills.push([require('@svg/skills/css.svg'), 'CSS3'])
+		arrSkills.push([require('@svg/skills/sass.svg'), 'SASS'])
+
+		arrSkills.push([require('@svg/skills/js.svg'), 'JavaScript'])
+		arrSkills.push([require('@svg/skills/ts.svg'), 'TypeScript'])
+		arrSkills.push([require('@svg/skills/gulp.svg'), 'Gulp'])
+		arrSkills.push([require('@svg/skills/react.svg'), 'React'])
+
+		arrSkills.push([require('@svg/skills/git.svg'), 'Git'])
+		arrSkills.push([require('@svg/skills/figma.svg'), 'Figma'])
+		arrSkills.push([require('@svg/skills/photoshop.svg'), 'Photoshop'])
+		arrSkills.push([require('@svg/skills/illustrator.svg'), 'Illustrator'])
+
+		distributeSkills('Использую:')
+
+		arrSkills.push([require('@svg/skills/vue.svg'), 'Vue'])
+		arrSkills.push([require('@svg/skills/redux.svg'), 'Redux'])
+		arrSkills.push([require('@svg/skills/webpack.svg'), 'Webpack'])
+		arrSkills.push([require('@svg/skills/english.svg'), 'Английский язык A1'])
+
+		distributeSkills('Изучаю:')
+
+		arrSkills.push([require('@svg/skills/java.svg'), 'Java'])
+		arrSkills.push([require('@svg/skills/android.svg'), 'Android'])
+		arrSkills.push([require('@svg/skills/csharp.svg'), 'C#'])
+		arrSkills.push([require('@svg/skills/mysql.svg'), 'MySQL'])
+
+		distributeSkills('Другие навыки:')
+
+		setArrBlocksSkills(arrBlocks)
+	}, [setArrBlocksSkills])
+
 	return (
 		<section id='skills' className={sass.skills}>
 			<Headline text='Навыки' classes={sass.headline} />
 
 			<div className={sass.blocks}>
-				<div className='block'>
-					<div className={sass.title}>Пользуюсь:</div>
+				{arrBlocksSkills.map(([nameTitle, arrFourSkills], i) => {
+					return (
+						<div className={sass.block} key={i}>
+							<div className={sass.title}>{nameTitle}</div>
 
-					<div className='skills-block'>
-						<div className='skill'>
-							<div className='wrap'>
-								{/* // TODO Вернуться к изображениям */}
-								<img src='' alt='' className='icon' />
+							<div className={sass['skills-block']}>
+								{arrFourSkills.map((fourSkills, i) => {
+									return (
+										<div className={sass['skills-row']} key={i}>
+											{fourSkills.map(([url, name], i) => {
+												return (
+													<div className={sass.skill} key={i}>
+														<Image url={url} classesWrap={sass.wrap} alt={name} />
+														<Label classes={sass.name}>{name}</Label>
+													</div>
+												)
+											})}
+										</div>
+									)
+								})}
 							</div>
-
-							<div className='name'></div>
 						</div>
-					</div>
-				</div>
+					)
+				})}
 			</div>
 		</section>
 	)
--- src/components/ui/text/label/Label.tsx
+++ src/components/ui/text/label/Label.tsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import sass from './Label.module.sass'
+import { mergeAllClasses } from '@utils/sassControl'
 
 interface PropsLabel {
-	type: 'welcome' | 'name' | 'profession' | undefined
 	children: string
+	type?: 'welcome' | 'name' | 'profession'
+	classes?: string
 }
 
-const Label: React.FC<PropsLabel> = ({ type, children }) => {
-	const [classLabel, setClassLabel] = useState<string>('')
+const Label: React.FC<PropsLabel> = ({ type, children, classes }) => {
+	const [allClassesLabel, setAllClassesLabel] = useState<string>(sass.usually)
 
 	useEffect(() => {
-		switch (type) {
-			case 'welcome':
-				setClassLabel(sass.hello)
-				break
-			case 'name':
-				setClassLabel(sass.name)
-				break
-			case 'profession':
-				setClassLabel(sass.who)
-				break
-			case undefined:
-			default:
-				setClassLabel(sass.usually)
-				break
+		const spotLabel = (clazz?: string): void => {
+			switch (type) {
+				case 'welcome':
+					setAllClassesLabel(mergeAllClasses([sass.hello], clazz))
+					break
+				case 'name':
+					setAllClassesLabel(mergeAllClasses([sass.name], clazz))
+					break
+				case 'profession':
+					setAllClassesLabel(mergeAllClasses([sass.who], clazz))
+					break
+				case undefined:
+				default:
+					setAllClassesLabel(mergeAllClasses([sass.usually], clazz))
+					break
+			}
 		}
-	}, [classLabel, type])
 
-	return type === 'name' ? <strong className={classLabel}>{children}</strong> : <div className={classLabel}>{children}</div>
+		if (!classes) {
+			spotLabel()
+		} else {
+			spotLabel(classes)
+		}
+	}, [classes, type])
+
+	return type === 'name' ? <strong className={allClassesLabel}>{children}</strong> : <div className={allClassesLabel}>{children}</div>
 }
 
 export default Label
