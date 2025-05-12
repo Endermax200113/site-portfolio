8bf577c1e629eee9795d513c3b032672f237ea56
Max Hallasaar <endermax200113@mail.ru>
2025-05-12 22:11:28 +0300

    Migrated from sass files to scss in the 'illustration' directory

--- src/components/ui/illustration/Illustration.tsx
+++ src/components/ui/illustration/Illustration.tsx
@@ -1,5 +1,5 @@
 import React, { HTMLAttributes } from 'react'
-import sass from './Illustration.module.sass'
+import scss from './Illustration.module.scss'
 import Image from '@ui/image/Image'
 import { mergeAllClasses } from '@utils/sassControl'
 import SVG from 'SVGImage'
@@ -14,13 +14,22 @@ interface PropsIllustration extends HTMLAttributes<HTMLElement> {
 }
 
 const Illustration: React.FC<PropsIllustration> = ({ classText, mergeClassText, classImage, mergeClassIllustration, className, image, children, ...props }) => {
-	const classIllustration: string = mergeAllClasses([mergeClassIllustration || !className ? sass.illustration : ''], className)
-	const allClassesText: string = mergeAllClasses([mergeClassText || !classText ? sass.text : ''], classText)
+	const classIllustration: string = mergeAllClasses([mergeClassIllustration || !className ? scss.illustration : ''], className)
+	const allClassesText: string = mergeAllClasses([mergeClassText || !classText ? scss.text : ''], classText)
 
 	return (
-		<figure className={classIllustration} {...props}>
-			<Image url={image} className={classImage} alt={children} />
-			<figcaption className={allClassesText} children={children} />
+		<figure
+			className={classIllustration}
+			{...props}>
+			<Image
+				url={image}
+				className={classImage}
+				alt={children}
+			/>
+			<figcaption
+				className={allClassesText}
+				children={children}
+			/>
 		</figure>
 	)
 }
