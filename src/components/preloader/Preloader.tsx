5e73c44d4d4fb9e48d12b938517550b613f7c232
Max Hallasaar <endermax200113@mail.ru>
2025-06-19 00:13:27 +0300

    The 'Preloader' component is developed

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -4,7 +4,15 @@ import scss from './Preloader.module.scss'
 interface PropsPreloader extends HTMLAttributes<HTMLDivElement> {}
 
 const Preloader: React.FC<PropsPreloader> = () => {
-	return <div className={scss.preload}>Preloader</div>
+	return (
+		<div className={scss.preload}>
+			<div className={scss.load}>
+				<div className={scss.first} />
+				<div className={scss.second} />
+				<div className={scss.third} />
+			</div>
+		</div>
+	)
 }
 
 export default Preloader
